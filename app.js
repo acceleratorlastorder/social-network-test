50120d6eff4b5b34a161b6e72d73883204b55c86
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-30 15:00:23 +0100

    update

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var myApp = angular.module('helloworld', ['ui.router']);
+var myApp = angular.module('routing', ['ui.router']);
 
 myApp.config(function($stateProvider) {
   var feedState = {
     name: 'feed',
     url: '/feed',
-    template: "<h3>hello world!</h3>"
+    templateUrl: 'partials/feed.html'
   }
-
   var loginState = {
     name: 'login',
     url: '/login',
-    template: '<h3>Its the UI-Router hello world app!</h3>'
+    templateUrl: 'partials/login.html'
   }
-
   $stateProvider.state(feedState);
   $stateProvider.state(loginState);
 });
