a02a5968e4da274d9c9ae7908b5c521d46485e8f
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-31 12:15:24 +0100

    updata

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-let testjson = 'ID:1,text:"lorem ipsum"';
-console.log("testjson is", testjson);
-JSON.parse(testjson);
-console.log("testjson after json is", testjson);
-JSON.stringify(testjson);
+let jasonobject = {
+    jason : {
+        name : "Jason Lengstorf",
+        age : "24",
+        gender : "male"
+    },
+    kyle : {
+        name : "Kyle Lengstorf",
+        age : "21",
+        gender : "shemale"
+    },
+    Bahamut : {
+        name : "bahamut mamouth",
+        age : "666",
+        gender : "male"
+    },
+    Odin : {
+        name : "odin zantetsuken",
+        age : "450",
+        gender : "male"
+    }
+
+}
+console.log("jasonobject ID is", jasonobject.Bahamut);
+console.log("jasonobject is", jasonobject);
+
+JSON.stringify(jasonobject)
+console.log("jasonobject after stringify is", jasonobject);
+JSON.parse(jasonobject)
+console.log("jasonobject after parse is", jasonobject);
+
+
+
+angular.module('inputExample', [])
+  .controller('ExampleController', ['$scope', function($scope) {
+    $scope.user = {name: 'guest', last: 'visitor'};
+  }]);
