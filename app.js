95506d860ca8283f8b9598c7ae6fe22e0afdf453
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-31 16:32:40 +0100

    update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*
+
 let jasonobject = {
     jason : {
         name : "Jason Lengstorf",
@@ -20,18 +22,4 @@ let jasonobject = {
         gender : "male"
     }
 
-}
-console.log("jasonobject ID is", jasonobject.Bahamut);
-console.log("jasonobject is", jasonobject);
-
-JSON.stringify(jasonobject)
-console.log("jasonobject after stringify is", jasonobject);
-JSON.parse(jasonobject)
-console.log("jasonobject after parse is", jasonobject);
-
-
-
-angular.module('inputExample', [])
-  .controller('ExampleController', ['$scope', function($scope) {
-    $scope.user = {name: 'guest', last: 'visitor'};
-  }]);
+}*/
--- router.js
+++ router.js
@@ -14,3 +14,113 @@ myApp.config(function($stateProvider) {
   $stateProvider.state(feedState);
   $stateProvider.state(loginState);
 });
+myApp.controller('ExampleController', ['$scope', function($scope) {
+  $scope.list = [];
+  $scope.submit = function() {
+    if ($scope.text) {
+      $scope.list.push(this.text);
+      $scope.text = '';
+    }
+  };
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let jasonobject = {
+    jason : {
+        name : "Jason Lengstorf",
+        age : "24",
+        gender : "male"
+    },
+    kyle : {
+        name : "Kyle Lengstorf",
+        age : "21",
+        gender : "shemale"
+    },
+    Bahamut : {
+        name : "bahamut mamouth",
+        age : "666",
+        gender : "male"
+    },
+    Odin : {
+        name : "odin zantetsuken",
+        age : "450",
+        gender : "male"
+    }
+
+}
