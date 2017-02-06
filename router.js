b455a9d49b6da84eff0ce6c04e94c83887d9c7b7
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-06 11:29:20 +0100

    update

--- router.js
+++ router.js
@@ -1,135 +1,38 @@
 var myApp = angular.module('routing', ['ui.router']);
 
 myApp.config(function($stateProvider) {
-  var feedState = {
-    name: 'feed',
-    url: '/feed',
-    templateUrl: 'partials/feed.html'
-  }
-  var loginState = {
-    name: 'login',
-    url: '/login',
-    templateUrl: 'partials/login.html'
-  }
-  $stateProvider.state(feedState);
-  $stateProvider.state(loginState);
+    var feedState = {
+        name: 'feed',
+        url: '/feed',
+        templateUrl: 'partials/feed.html'
+    }
+    var loginState = {
+        name: 'login',
+        url: '/login',
+        templateUrl: 'partials/login.html'
+    }
+    $stateProvider.state(feedState);
+    $stateProvider.state(loginState);
 });
 myApp.controller('ExampleController', ['$scope', function($scope) {
-  $scope.list = ["Eat", "Drink", "Breath", "Run", "Laugh", "Playing"];
-  $scope.submit = function() {
-    if ($scope.text) {
-      $scope.list.push(this.text);
-      $scope.text = '';
-    }
-/*
-    $scope.addItem = function(item) {
-        if (item === null || item === undefined || item === "") {
-            return;
+    $scope.list = ["lol", "lel", "salut"];
+    $scope.tweet = localStorage.getItem(angular.fromJson("tweet"));
+    $scope.submit = function() {
+        if ($scope.text) {
+            $scope.list.push(this.text);
+            localStorage.setItem("tweet", angular.toJson(this.text));
+            $scope.text = '';
+            console.log(localStorage.getItem("tweet"));
         }
-        $scope.list.push(item);
-        $scope.newItem = null;
-        save($scope.list);
-    };*/
-  };
+
+    };
+
 }]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let jasonobject = {
-    jason : {
-        name : "Jason Lengstorf",
-        age : "24",
-        gender : "male"
-    },
-    kyle : {
-        name : "Kyle Lengstorf",
-        age : "21",
-        gender : "shemale"
-    },
-    Bahamut : {
-        name : "bahamut mamouth",
-        age : "666",
-        gender : "male"
-    },
-    Odin : {
-        name : "odin zantetsuken",
-        age : "450",
-        gender : "male"
-    }
-
+saveToStorage = function() {
+   localStorage.setItem("tweet", JSON.stringify(this.text));
+ }
+
+ loadFromStorage = function() {
+   this.text = JSON.parse(localStorage.getItem("tweet"));
 }
