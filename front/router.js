e83b9fd4460164e30b5a76d9c511ce85ca0a7603
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-07 12:14:35 +0100

    update

--- front/router.js
+++ front/router.js
@@ -14,8 +14,39 @@ myApp.config(function($stateProvider) {
     $stateProvider.state(feedState);
     $stateProvider.state(loginState);
 });
-myApp.controller('ExampleController', ['$scope', '$http', function($scope, $http) {
-    $scope.list = ["lol", "lel", "salut"];
+myApp.controller('ExampleController', ['$scope', '$http', "$q", function($scope, $http, $q) {
+
+    // Simple GET request example:
+    $http({
+        method: 'GET',
+        url: 'feed.json'
+    }).then(function successCallback(response, data, status) {
+        $scope.tweets = response.data.tweets;
+
+        let messages = $scope.tweets[0].message;
+
+        console.log("succes on the json get");
+        console.log("response: ", response);
+        console.log("status: ", response.status);
+        console.log("data: ", response.data);
+        console.log("tweets: ", response.data.tweets);
+        console.log("tweet 1: ", response.data.tweets[0]);
+        console.log("tweet 2: ", response.data.tweets[1]);
+        console.log("tweet 3: ", response.data.tweets[2]);
+        console.log("tweet 3: ", response.data.tweets[3]);
+        console.log("test: ", $scope.tweets);
+        console.log("message", messages);
+
+
+        feedthepage($scope.tweets);
+
+
+    }, function errorCallback(response) {
+        console.log("error");
+        document.getElementById("erreur").innerHTML = "Erreur lors de l'appel du json"
+    });
+
+    $scope.list = ["lol"];
     $scope.tweet = localStorage.getItem("tweet");
     $scope.salut = "";
     $scope.submit = function() {
@@ -24,23 +55,58 @@ myApp.controller('ExampleController', ['$scope', '$http', function($scope, $http
             localStorage.setItem("tweet", angular.toJson(this.text));
             $scope.text = '';
             console.log(localStorage.getItem("tweet"));
-            console.log($scope.tweet);
         }
 
     };
-    $http.get("feezd.json").
-    then(function(data, status) {
-        $scope.donnees = data;
-    }).
-    error(function(data, status) {
-        document.getElementById("erreur").innerHTML = "Erreur lors de l'appel du json"
-    });
+
+
+    function feedthepage(x) {
+        console.log("feedthepage function, tweets:", x);
+        let i = 0;
+        for (i = 0; i < x.length; i++) {
+            console.log(i);
+            $scope.list.push($scope.tweets[i].message);
+        }
+
+        console.log("nombre de tweets :", i);
+    }
+
+
+
 }]);
 
+
+
+
+
+
+
+
+
+/*
 saveToStorage = function() {
     localStorage.setItem("tweet", JSON.stringify(this.text));
 }
 
 loadFromStorage = function() {
     this.text = JSON.parse(localStorage.getItem("tweet"));
-}
+}  */
+
+
+
+/*
+
+
+
+$http.get("feed.json").$q.then(function(data, status) {
+  $scope.donnees = data;
+}).
+error(function(data, status) {
+  document.getElementById("erreur").innerHTML = "Erreur lors de l'appel du json"
+});
+
+
+
+
+
+*/
