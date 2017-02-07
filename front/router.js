74f753c873663cde25fb91c1ed83abc791f50992
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-07 10:15:30 +0100

    optimisation of the working folder placing the front section of the website is in the /front folder and the back in the /back which will make it easier for the next and also making the working directory easier to read

--- front/router.js
+++ front/router.js
@@ -14,25 +14,33 @@ myApp.config(function($stateProvider) {
     $stateProvider.state(feedState);
     $stateProvider.state(loginState);
 });
-myApp.controller('ExampleController', ['$scope', function($scope) {
+myApp.controller('ExampleController', ['$scope', '$http', function($scope, $http) {
     $scope.list = ["lol", "lel", "salut"];
-    $scope.tweet = localStorage.getItem(angular.fromJson("tweet"));
+    $scope.tweet = localStorage.getItem("tweet");
+    $scope.salut = "";
     $scope.submit = function() {
         if ($scope.text) {
             $scope.list.push(this.text);
             localStorage.setItem("tweet", angular.toJson(this.text));
             $scope.text = '';
             console.log(localStorage.getItem("tweet"));
+            console.log($scope.tweet);
         }
 
     };
-
+    $http.get("feezd.json").
+    then(function(data, status) {
+        $scope.donnees = data;
+    }).
+    error(function(data, status) {
+        document.getElementById("erreur").innerHTML = "Erreur lors de l'appel du json"
+    });
 }]);
 
 saveToStorage = function() {
-   localStorage.setItem("tweet", JSON.stringify(this.text));
- }
+    localStorage.setItem("tweet", JSON.stringify(this.text));
+}
 
- loadFromStorage = function() {
-   this.text = JSON.parse(localStorage.getItem("tweet"));
+loadFromStorage = function() {
+    this.text = JSON.parse(localStorage.getItem("tweet"));
 }
